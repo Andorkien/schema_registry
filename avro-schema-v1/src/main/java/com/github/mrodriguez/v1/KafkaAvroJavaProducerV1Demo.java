44be1f183c9624399ced309aa94aa932cf21fa5f
Manuel <manuelignacio.calvo@gmail.com>
2020-12-15 11:36:47 -0300

    commit1

---
+++ avro-schema-v1/src/main/java/com/github/mrodriguez/v1/KafkaAvroJavaProducerV1Demo.java
@@ -0,0 +1,60 @@
+package com.github.mrodriguez.v1;
+
+import com.example.Customer;
+import com.hortonworks.registries.schemaregistry.client.SchemaRegistryClient;
+import com.hortonworks.registries.schemaregistry.serdes.avro.kafka.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaAvroJavaProducerV1Demo {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        // normal producer
+        properties.put("bootstrap.servers", "c389-node4.coelab.cloudera.com:9092");
+        properties.put("acks", "all");
+        properties.put("retries", "10");
+        // avro part
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
+        properties.put("security.protocol", "SASL_PLAINTEXT");
+        //properties.setProperty("schema.registry.url", "http://c389-node4.coelab.cloudera.com:7788");
+        properties.putAll(Collections.singletonMap(SchemaRegistryClient.Configuration.SCHEMA_REGISTRY_URL.name(),"http://c389-node4.coelab.cloudera.com:7788/api/v1/"));
+        Producer<String, Customer> producer = new KafkaProducer<String, Customer>(properties);
+
+        String topic = "schema1";
+
+        // copied from avro examples
+        Customer customer = Customer.newBuilder()
+                .setAge(34)
+                .setAutomatedEmail(false)
+                .setFirstName("John")
+                .setLastName("Doe")
+                .setHeight(178f)
+                .setWeight(75f)
+                .build();
+
+        ProducerRecord<String, Customer> producerRecord = new ProducerRecord<String, Customer>(
+                topic, customer
+        );
+
+        System.out.println(customer);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception exception) {
+                if (exception == null) {
+                    System.out.println(metadata);
+                } else {
+                    exception.printStackTrace();
+                }
+            }
+        });
+
+        producer.flush();
+        producer.close();
+
+    }
+}
---
+++ avro-schema-v2/src/main/java/com/github/mrodriguez/v2/KafkaAvroJavaConsumerV2Demo.java
@@ -0,0 +1,50 @@
+package com.github.mrodriguez.v2;
+
+import com.example.Customer;
+import com.hortonworks.registries.schemaregistry.client.SchemaRegistryClient;
+import com.hortonworks.registries.schemaregistry.serdes.avro.AbstractAvroSnapshotDeserializer;
+import com.hortonworks.registries.schemaregistry.serdes.avro.kafka.KafkaAvroDeserializer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Collections;
+import java.util.Properties;
+
+public class KafkaAvroJavaConsumerV2Demo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        // normal consumer
+        properties.put("bootstrap.servers","c389-node4.coelab.cloudera.com:9092");
+        properties.put("group.id", "schema_topic-v2");
+        properties.put("security.protocol", "SASL_PLAINTEXT");
+        properties.put("auto.commit.enable", "false");
+        properties.put("auto.offset.reset", "earliest");
+        //properties.put("schema.registry.url", "http://c389-node4.coelab.cloudera.com:7788/api/v1/");
+
+        // avro part (deserializer)
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
+        properties.putAll(Collections.singletonMap(SchemaRegistryClient.Configuration.SCHEMA_REGISTRY_URL.name(),"http://c389-node4.coelab.cloudera.com:7788/api/v1/"));
+        properties.put(AbstractAvroSnapshotDeserializer.SPECIFIC_AVRO_READER, true);
+
+        KafkaConsumer<String, Customer> kafkaConsumer = new KafkaConsumer<>(properties);
+        String topic = "schema1";
+        kafkaConsumer.subscribe(Collections.singleton(topic));
+
+        System.out.println("Waiting for data...");
+
+        while (true){
+            ConsumerRecords<String, Customer> records = kafkaConsumer.poll(1000);
+
+            for (ConsumerRecord<String, Customer> record : records){
+                Customer customer = record.value();
+                System.out.println(customer);
+            }
+
+            kafkaConsumer.commitSync();
+        }
+    }
+}
